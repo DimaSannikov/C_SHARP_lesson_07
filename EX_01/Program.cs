afc5f8c2a0acffa775ded67e3b030d32c70524f7
DimaSannikov <dimasannikov@gmail.com>
2023-03-20 12:09:55 +0500

    added EX_01

---
+++ EX_01/Program.cs
@@ -0,0 +1,52 @@
+﻿// string NubbersFor(int a, int b)
+// {
+//     string result = String.Empty;
+//     for (int i = a; i <= b; i++)
+//     // for (int i = b; i >= a; i--)
+//     {
+//         result += $"{i} ";
+//     }
+//     return result;
+// }
+
+// string NubbersRec(int a, int b)
+// {
+//     if (a <= b) return $"{a} " + NubbersRec(a + 1, b);
+//     // if (a <= b) return NubbersRec(a + 1, b) + $"{a} ";
+//     else return String.Empty;
+// }
+
+// Console.WriteLine(NubbersFor(1, 10));
+// Console.WriteLine(NubbersRec(1, 10));
+
+// int SumFor(int n)
+// {
+//     int result = 0;
+//     for (int i = 0; i <= n; i++) result += i;
+//     return result;
+// }
+
+// int SumRec(int n)
+// {
+//     if (n == 0) return 0;
+//     else return n + SumRec(n - 1);
+// }
+
+// Console.WriteLine(SumFor(10));
+// Console.WriteLine(SumRec(10));
+
+int FactorialFor(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= i;
+    return result;
+}
+
+int FactorialRec(int n)
+{
+    if (n == 1) return 1;
+    else return n * FactorialRec(n - 1);
+}
+
+Console.WriteLine(FactorialFor(10));
+Console.WriteLine(FactorialRec(10));
