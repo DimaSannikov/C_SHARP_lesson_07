a321656753dba6007e02bb15f5697167fe9956a0
DimaSannikov <dimasannikov@gmail.com>
2023-03-20 12:10:21 +0500

    added EX_03

---
+++ EX_03/Program.cs
@@ -0,0 +1,31 @@
+﻿// string path = "/home/ds/Downloads";
+// DirectoryInfo di = new DirectoryInfo(path);
+// System.Console.WriteLine(di.CreationTime);
+// FileInfo[] fi = di.GetFiles();
+
+// for (int i = 0; i < fi.Length; i++)
+// {
+//     System.Console.WriteLine(fi[i].Name);
+// }
+
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + "    ");
+    }
+    
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+
+string path = @"/home/ds/Downloads/Microsoft Windows 7 SP1 COEM CIS and GE";
+CatalogInfo(path);
